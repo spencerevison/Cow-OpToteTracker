a0dd7a9a67c1dfa2da124a4a0bad4bb9c51ad35f
Spencer Campbell <spence@twentyhertz.com>
2022-08-18 12:47:38 -0700

    Support archiving records

--- netlify/functions/delete-record/delete-record.ts
+++ netlify/functions/delete-record/delete-record.ts
@@ -7,13 +7,40 @@ const client = contentful.createClient({
 
 export const handler: Handler = async (event) => {
   const { toteId } = JSON.parse(event.body);
+  let env;
 
   try {
     await client
       .getSpace(process.env.CONTENTFUL_SPACE_ID)
       .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT))
-      .then((environment) => environment.getEntry(toteId))
-      .then((entry) => entry.archive());
+      .then((environment) => {
+        env = environment;
+        return environment.getEntry(toteId);
+      })
+      .then((entry) => {
+        // Create archived record
+        const id = `${entry.fields.toteId["en-US"]}-${
+          entry.fields.orderId["en-US"]
+        }-${Date.now()}`;
+        env.createEntryWithId("tote", id, {
+          fields: {
+            customerName: {
+              "en-US": entry.fields.customerName["en-US"],
+            },
+            orderId: {
+              "en-US": entry.fields.orderId["en-US"],
+            },
+            toteId: {
+              "en-US": entry.fields.toteId["en-US"],
+            },
+            archived: {
+              "en-US": true,
+            },
+          },
+        });
+        //return entry.unpublish().then((entry) => entry.delete());
+        return entry.delete();
+      });
     return {
       statusCode: 200,
     };
