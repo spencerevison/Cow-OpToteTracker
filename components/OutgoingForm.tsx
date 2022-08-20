9e44b74baaa222e6ea07a85685f9ed82adaf2c4f
Spencer Campbell <spence@twentyhertz.com>
2022-08-19 18:59:55 -0700

    Debugging contentful env

--- components/OutgoingForm.tsx
+++ components/OutgoingForm.tsx
@@ -13,6 +13,8 @@ interface FormInputs {
   toteId: string;
 }
 
+console.log(process.env.CONTENTFUL_ENVIRONMENT);
+
 const OutgoingForm = () => {
   // Get names for autocomplete
   const [names, setNames] = useState([]);
